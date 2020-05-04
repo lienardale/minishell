8be93bd1eccfd4e7bf7c3b2862d09d84dbe6bfd7
lienardale <alex59.facebook@laposte.net>
2020-05-04 14:18:05 +0200

    cdai's execve fct added

--- v0_process.c
+++ v0_process.c
@@ -6,35 +6,86 @@
 /*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 08:11:25 by alienard          #+#    #+#             */
-/*   Updated: 2020/05/02 08:16:05 by alienard         ###   ########.fr       */
+/*   Updated: 2020/05/04 12:15:00 by alienard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "v0_minishell.h"
+
+char	*ft_get_onlypaths(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (!ft_strncmp(env[i], "PATH=", 5))
+			return (ft_strtrim(env[i], "PATH="));
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_findexec(char *path, char *exec)
+{
+	DIR				*dir;
+	struct dirent	*tmp;
+	char			*result;
+	int				len;
+
+	result = NULL;
+	dir = opendir(path);
+	while ((tmp = readdir(dir)))
+	{
+		if (ft_strcmp(tmp->d_name, exec) == 0)
+		{
+			len = ft_strlen(path) + ft_strlen(exec) + 2;
+			result = ft_calloc(1, len);
+			ft_strlcat(result, path, len);
+			ft_strlcat(result, "/", len);
+			ft_strlcat(result, exec, len);
+			closedir(dir);
+			return (result);
+		}
+	}
+	closedir(dir);
+	return (result);
+}
+
+char	*ft_get_abspath_filename(char *exec, char **env)
+{
+	char	*tmp;
+	char	**paths;
+	char	*result;
+	int		i;
+
+	i = 0;
+	tmp = ft_get_onlypaths(env);
+	paths = ft_split(tmp, ':');
+	while (paths[i])
+	{
+		if ((result = ft_findexec(paths[i], exec)))
+			return (result);
+		i++;
+	}
+	return (result);
+}
 
 int		ft_launch(char **args, char **env)
 {
 	pid_t	pid;
 	pid_t	wpid;
 	int		status;
-	int		i;
-	// char	*tmp;
-	// char	**envp;
 
 	pid = fork();
 	if (pid == 0)
 	{
 		// Child process
-		// if (execvp(args[0], args) == -1)
-		// tmp = ft_strtrim(env[1], "PATH=");
-		// envp = ft_split(tmp, ':');
-		args[0] = "/bin/ls";
-		for (i = 0; args[i] != NULL; i++)
-			ft_printf("%s\n", args[i]);
+		args[0] = (ft_get_abspath_filename(args[0], env));
 		if (execve(args[0], args, env) == -1)
 		{
-			// ft_dprintf(2, "error execve\n");
-			perror("error execve");
+			ft_dprintf(2, "error execve\n");
+			// perror("error execve");
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -51,5 +102,5 @@ int		ft_launch(char **args, char **env)
 		while (!WIFEXITED(status) && !WIFSIGNALED(status))
 			wpid = waitpid(pid, &status, WUNTRACED);
 	}
-	return 1;
+	return (1);
 }
