c0919e79b6c5c409f7f376823d3769a169844f28
cdai <cdai@student.42.fr>
2020-05-06 13:56:42 +0000

    Title:		Add in comment theory in v0_prompt.c

--- v0_prompt.c
+++ v0_prompt.c
@@ -19,14 +19,25 @@ void	ft_prompt(int *check, int fd, char **env)
 	int		ret;
 	int		i;
 	static int	(*builtin_fct[])(char **) = {BUILTINS};
+	// char **prompt;
+	// booleen quote;
 
+
+	// booleen = 0;
+	//prompt = malloc(sizeof(*char)); // liste chainee ?
 	ret = 1;
 	while (ret && (write(1,">",1)) && (*check = get_next_line(fd, &line)) >= 0)
 	{
 		i = 0;
+		// prompt[0] /*normalement c'est j*/ = line;
+		// j++; ou maillon suivant ?
+		// quote = ft_check_line(line, quote); //ternaire ?
+		// if (!quote) {
 		args = ft_split_line(line);
-		ft_free_ptr(line);
 		ret = ft_parse_line(args, env, builtin_fct);
+		// ft_free_prompt(prompt);
+		// }
+		ft_free_ptr(line);
 		while (args[i])
 			ft_free_ptr(args[i++]);
 		ft_free_ptr(args);
