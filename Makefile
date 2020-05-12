8c3a48c7a4562941d6dce4841509c78a1fb12cbe
Dai Clement <cdai@student.42.fr>
2020-05-12 08:45:10 +0000

    Title:		Big Update on v0_parse_path.c. Content:	Add function ft_lst_minishell_join (not finished). 			static char	*ft_lst_minishell_join(t_list *lst); 				//futur(ft_lst_minishell_join(t_list *lst, char *exec)). 				Join all content element from lst with '/' character between each element. 				// And join exec to the path. Then return. 				Return NULL if malloc fails. 				Small update on Makefile and .gitignore.

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: alienard <alienard@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/09 18:51:33 by alienard          #+#    #+#              #
-#    Updated: 2020/05/11 14:20:41 by cdai             ###   ########.fr        #
+#    Updated: 2020/05/12 08:22:18 by cdai             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -119,5 +119,9 @@ config_alienard:
 	git config --global user.email "alienard@student.42.fr";
 	git config --global user.name "Lienard Alexandre";
 	sed 's/marvin/alienard/' 42header/vim/stdheader.vim | sed 's/42.fr/student.42.fr/' > ~/.vim/plugin/stdheader.vim;
+
+cdai:
+	clang v0_parse_path.c libft/libft.a -I./libft/;
+	./a.out;
 
 .PHONY:		re all clean fclean libft_test test build run exec kill config_cdai config_alienard
--- v0_parse_path.c
+++ v0_parse_path.c
@@ -6,38 +6,69 @@
 /*   By: cdai <cdai@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/10 09:31:04 by cdai              #+#    #+#             */
-/*   Updated: 2020/05/10 10:14:17 by cdai             ###   ########.fr       */
+/*   Updated: 2020/05/12 08:40:27 by cdai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "v0_minishell.h"
 
-int	ft_is_cwd(char *exec)
+static int	ft_is_cwd(char *exec)
 {
 	if (exec[0] == '.' && (exec[1] == '/' || exec[1] == '\0'))
 		return (1);
 	return (0);
 }
 
-int	ft_is_parent_dir(char *exec)
+static int	ft_is_parent_dir(char *exec)
 {
 	int	i;
 
 	i = 0;
 	while (exec[i] == '.')
 		i++;
-	if (i == 2 && (exec[i] == '/' || exec[i] == '\0')
+	if (i == 2 && (exec[i] == '/' || exec[i] == '\0'))
 		return (1);
 	return (0);
 }
 
-char	*ft_parse_path(char *exec)
+static char	*ft_lst_minishell_join(t_list *lst)
 {
-	char *buff;
-	int i;
+	char	*result;
+	char	*temp;
+
+	result = 0;
+	while(lst)
+	{
+		if (result)
+		{
+			temp = result;
+			result = ft_strjoin(temp, "/");
+			free(temp);
+			temp = result;
+			result = ft_strjoin(temp, (char*)lst->content);
+			free(temp);
+		}
+		else
+			result = ft_strjoin("/", (char*)lst->content);
+		lst = lst->next;
+	}
+	return (result);
+}
+
+char		*ft_parse_path(char *exec)
+{
+	t_list	*exec_splited;
+	char	*result;
+	char	*buff;
+	t_list	*buff_splited;
+	int		i;
 
 	i = 0;
+	exec_splited = ft_lstsplit(exec, '/');
 	buff = ft_calloc(1, 1000);
+	getcwd(buff, 1000);
+	buff_splited = ft_lstsplit(buff, '/');
+result = 0;
 /*
 	while (exec[i])
 	{
@@ -55,14 +86,28 @@ char	*ft_parse_path(char *exec)
 			i++;
 	}
 */
+	result = ft_lst_minishell_join(buff_splited);
 	return (result);
 }
 
 int main()
 {
 	char *buff;
+	t_list	*splited;
+	t_list	*temp_splited;
 
 	buff = ft_calloc(1, 1000);
 	printf("from main: %s\n", (buff = getcwd(buff, 50)));
+	splited = ft_lstsplit(buff, '/');
+	temp_splited = splited;
+	while (temp_splited)
+	{
+		printf("lst value: %s\n", (char*)temp_splited->content);
+		temp_splited = temp_splited->next;
+	}
+	char *test = ft_lst_minishell_join(splited);
+	printf("%s\n", test);
+	ft_lstclear(&splited, free);
+	
 	free(buff);
 }
