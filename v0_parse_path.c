6635dec32c869aa8bf1938c7712bb7c896cc7703
Dai Clement <cdai@student.42.fr>
2020-05-12 15:35:07 +0000

    Title:		Fix bug update. Content:	Fix leaks. 			Everythings work well.

--- v0_parse_path.c
+++ v0_parse_path.c
@@ -6,7 +6,7 @@
 /*   By: cdai <cdai@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/10 09:31:04 by cdai              #+#    #+#             */
-/*   Updated: 2020/05/12 12:44:12 by cdai             ###   ########.fr       */
+/*   Updated: 2020/05/12 15:31:50 by cdai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,30 @@ static	int	ft_isolate_exec(char *path, char **buff)
 
 static t_list	*ft_relative_to_abs_path(t_list *lstdest, t_list *lstsrc)
 {
+	char	*temp;
+	t_list	*lst_temp;
+
 	while (lstsrc)
 	{
 		if (ft_strcmp("..", (char*)lstsrc->content) == 0)
 			ft_lstdel_last(lstdest);
 		else if (ft_strcmp(".", (char*)lstsrc->content) != 0)
-			ft_lstadd_back(&lstdest, lstsrc);
+		{
+			temp = ft_strdup((char*)lstsrc->content);
+			lst_temp = ft_lstnew(temp);
+//			free(temp);
+			ft_lstadd_back(&lstdest, lst_temp);
+		}
 		lstsrc = lstsrc->next;
 	}
+	return (lstdest);
 }
 
 char		*ft_parse_path(char *path)
 {
 	t_list	*path_splited;
 	char	*result;
+	char	*temp;
 t_list	*buff_splited;
 	char	*exec;
 	int		start;
@@ -52,19 +62,27 @@ t_list	*buff_splited;
 
 
 	start = ft_isolate_exec(path, &exec);
-path = ft_substr(path, 0, start - 1);
+path = ft_substr(path, 0, start);
 path_splited = ft_lstsplit(path, '/');
 free(path);
 
 // maybe do a function for readability.
 result = ft_calloc(1, 1000);
 getcwd(result, 1000);
-buff_splited = ft_lstsplit(buff, '/');
+buff_splited = ft_lstsplit(result, '/');
 free(result);
 
 //	t_list *ft_relative_to_abs_path(t_list *lstdest, t_list *lstsrc);
 	ft_relative_to_abs_path(buff_splited, path_splited);
 	result = ft_lststrjoin(buff_splited, "/");
+	temp = result;
+	result = ft_strjoin(result, "/");
+	free(temp);
+	temp = result;
+	result = ft_strjoin(result, exec);
+	free(temp);
+
+	free(exec);
 	// join de exec
 	ft_lstclear(&buff_splited, free);
 	ft_lstclear(&path_splited, free);
