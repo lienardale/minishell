30893561cef78dc77f4d569d76fdcc2ad41da370
Dai Clement <dai.clement.perso@gmail.com>
2020-05-13 20:16:14 +0200

    Title:		Tiny Tiny update.

--- v0_process.c
+++ v0_process.c
@@ -6,7 +6,7 @@
 /*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 08:11:25 by alienard          #+#    #+#             */
-/*   Updated: 2020/05/18 15:46:34 by cdai             ###   ########.fr       */
+/*   Updated: 2020/05/18 15:48:17 by cdai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ static void	ft_search_n_execute(char **args, char **env)
 		args[0] = (ft_get_abspath_filename(args[0], env));
 	if (exec)
 		free(exec);
-	else if (!args[i])
+	else if (!args[0])
 	{
 		ft_dprintf(2, "ft_search_n_execute error?\n");
 		// perror("have to change error");
