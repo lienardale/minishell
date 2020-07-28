348b3b59b34376930327c9a01375d28831ecf27c
Clement Dai <cdai@student.42.fr>
2020-07-28 15:28:10 +0200

    Title:		Update v0_exit.c, add ft_is_in_min_max_atoi_long.c, ft_atoi_long.c, ft_str_isdigit.c. Content:	ft_exit() handles return value. (All in comments). 			int		ft_is_in_min_max_atoi_long(const char *str); 				Return 1 if str is in range of long max-min. Overwise return 0. 			long	ft_atoi_long(const char *str); 				Return long type of str. 			int		ft_str_isdigit(char *str); 				Return 1 is str has only digit characters with only 1 '-' character before digits. 				If str has 2 '-' characters, return 0. 				Overwise return 0. 			Update libft/Makefile, libft/headers/libft.h.

--- srcs/v0_exit.c
+++ srcs/v0_exit.c
@@ -6,20 +6,46 @@
 /*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 14:40:49 by alienard          #+#    #+#             */
-/*   Updated: 2020/08/05 12:40:01 by cdai             ###   ########.fr       */
+/*   Updated: 2020/08/05 13:59:45 by cdai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "v0_minishell.h"
 
-int		ft_exit(t_cmd *cmd, t_sh *sh)
+/*
+static int	ft_is_double_minus(char *str)
 {
 	int	i;
 
 	i = 0;
+	while (str[i] == '-')
+		i++;
+	if (i == 2 && str[i] == 0)
+		return (1);
+	return (0);
+}
+*/
+
+int		ft_exit(t_cmd *cmd, t_sh *sh)
+{
+	int		i;
+//	int		return_value;
+
+	i = 0;
+	/*
+	if (!args)
+	{
+		ft_free_double_array(cmd->av);
+		ft_lstclear(env, ft_free_env_lst);
+		ft_printf("exit\n");
+		exit(1);
+	}
+*/
 	while (cmd->av[i])
 		i++;
 	// ft_printf("			-> %s passe bien par ici.\n", args[0]);
+// on devrait peut-etre faire une fontion free_minishell() ?
+
 	ft_free_double_array(cmd->av);
 	ft_lstclear(sh->env, ft_free_env_lst);
 /*
