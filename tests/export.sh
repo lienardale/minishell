727d737912e7dcb69ce80a67c0b25f057f7f06c6
lienardale <alex59.facebook@laposte.net>
2020-08-29 16:42:08 +0200

    solving vscode bullshit

--- tests/export.sh
+++ tests/export.sh
@@ -7,62 +7,62 @@ env | grep coucou
 unset coucou
 echo
 
-echo -n 0 : \	\
+echo -n 0 : \	\	
 export cou=ec ; export bon=ho ; $cou$bon salut
 echo "expected : 	salut"
 echo
 
-echo -n 1 : \	\
+echo -n 1 : \	\	
 export cou=ec ; export bon=ho ; "$cou$bon" salut
 echo "expected : 	salut"
 echo
 
-echo -n 2 : \	\
+echo -n 2 : \	\	
 export cou=ec ; export bon=ho ; '$cou$bon' salut
 echo "expected : 	 \$cou\$bon : command not found"
 echo
 
-echo -n 3 : \	\
+echo -n 3 : \	\	
 export cou=ec ; export bon=ho ; \$cou\$bon salut
 echo "expected : 	 \$cou\$bon : command not found"
 echo
 
-echo -n 4 : \	\
+echo -n 4 : \	\	
 export cou=ec ; export bon=ho ; \'$cou$bon\' salut
 echo "expected : 	 'echo' : command not found"
 echo
 
-echo -n 5 : \	\
+echo -n 5 : \	\	
 export cou=ec ; export bon=ho ; \"$cou$bon\" salut
 echo "expected : 	 \"echo\" : command not found"
 echo
 
-echo -n 6 : \	\
+echo -n 6 : \	\	
 export cou=ec ; export bon=ho ; "\$cou\$bon" salut
 echo "expected : 	 \$cou\$bon : command not found"
 echo
 
-echo -n 7 : \	\
+echo -n 7 : \	\	
 export cou=ec ; export bon=ho ; '\$cou\$bon' salut
 echo "expected : 	 \\\$cou\\\$bon : command not found"
 echo
 
-echo -n 8 : \	\
+echo -n 8 : \	\	
 export cou=ec ; export bon=ho ; $cou $bon salut
 echo "expected : 	 ec : command not found"
 echo
 
-echo -n 9 : \	\
+echo -n 9 : \	\	
 export cou=ec ; export bon=ho ; "$cou $bon" salut
 echo "expected : 	 ec ho : command not found"
 echo
 
-echo -n 10 : \	\
+echo -n 10 : \	\	
 export cou=ec ; export bon=ho ; $cou\ $bon salut
 echo "expected : 	 ec ho : command not found"
 echo
 
-echo -n 11 : \	\
+echo -n 11 : \	\	
 export cou=ec ; export bon=ho ; "$cou\ $bon" salut
 echo "expected : 	 ec\\ ho : command not found"
 echo
